2c7afb25112834c7149c84cb164d6444759a22ba
trinhha <trinh.the.ha@mgm-tp.com>
2019-03-07 09:03:33 +0700

    test repo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return
+    {
+      <div className="App">
+        this is a test page.
+      </div>;
+    }
   }
 }
 
