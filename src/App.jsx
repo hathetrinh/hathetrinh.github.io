9887f8636e754760cd6f82ff52df2248522cc011
trinhha <trinh.the.ha@mgm-tp.com>
2019-03-07 09:22:10 +0700

    update test template

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,209 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return
-    {
-      <div className="App">
-        this is a test page.
-      </div>;
-    }
+    return (
+      <div>
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />{' '}
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+        <hr />
+        <img
+          class="thumbnail-img"
+          id="colorSubsetImgId-1579134"
+          alt="Deerberg Armband Rakitall"
+          data-src="https://deerberg.scene7.com/is/image/deerberg/1579134?wid=45&amp;hei=58"
+          onerror="this.style.display='none';this.onerror=null;"
+        />
+      </div>
+    );
   }
 }
 
